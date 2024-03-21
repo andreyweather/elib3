4ad7864c1ca612398cfc2157f409197fbb12a6c3
andreyPogodin <andreyweather@gmail.com>
2024-03-21 12:18:23 +0300

    commit

---
+++ elib3/elib3.h
@@ -0,0 +1,18 @@
+//
+//  elib3.h
+//  elib3
+//
+//  Created by andrey.pogodin on 21.03.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for elib3.
+FOUNDATION_EXPORT double elib3VersionNumber;
+
+//! Project version string for elib3.
+FOUNDATION_EXPORT const unsigned char elib3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <elib3/PublicHeader.h>
+
+
